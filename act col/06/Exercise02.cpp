0974db5412d23fc3c25b3f4a9c906c2c1840fd7e
SamOctGonzAzpeitia <A01704696@itesm.mx>
2023-10-19 16:23:55 -0600

    add exercise 2 and 3

---
+++ act col/06/Exercise02.cpp
@@ -0,0 +1,58 @@
+// =================================================================
+//
+// File: exercise02.cpp
+// Author(s):
+//      Martin Adrian Noboa Monar - A01704052
+//      Samuel Octavio González Azpeitia - A01704696
+// Description: This file contains the code to brute-force all
+//				prime numbers less than MAXIMUM using the TBB 
+//				technology. To compile:
+//				g++ exercise02.cpp -o app -I/usr/local/lib/tbb/include -L/usr/local/lib/tbb/lib/intel64/gcc4.4 -ltbb
+//
+// Reference:
+// 	Read the document "exercise02.pdf"
+//
+// Copyright (c) 2022 by Tecnologico de Monterrey.
+// All Rights Reserved. May be reproduced for any non-commercial
+// purpose.
+//
+// =================================================================
+
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <tbb/blocked_range.h>
+#include <tbb/parallel_reduce.h>
+#include "utils.h"
+
+using namespace std;
+using namespace std::chrono;
+using namespace tbb;
+
+#define MAXIMUM 1000001 //1e6
+
+// implement your code
+
+int main(int argc, char* argv[]) {
+	double result;
+	// These variables are used to keep track of the execution time.
+	high_resolution_clock::time_point start, end;
+	double timeElapsed;
+
+	cout << "Starting...\n";
+	timeElapsed = 0;
+	for (int j = 0; j < N; j++) {
+		start = high_resolution_clock::now();
+
+		// call the implemented function
+
+		end = high_resolution_clock::now();
+		timeElapsed += 
+			duration<double, std::milli>(end - start).count();
+	}
+	cout << "result = " << result << "\n";
+	cout << "avg time = " << fixed << setprecision(3) 
+		 << (timeElapsed / N) <<  " ms\n";
+
+	return 0;
+}
